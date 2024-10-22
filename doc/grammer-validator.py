4b2be9b8856a42b1d2840f282b35889a42074a42
Ryu-Okamoto <r-okamot@ist.osaka-u.ac.jp>
2024-10-23 02:33:44 +0900

    add grammer text and a grammer validator

---
+++ doc/grammer-validator.py
@@ -0,0 +1,96 @@
+## validate whether the grammer is strict-LL1 grammer
+## strict-LL1 grammer is defined as the grammer without
+##   - body that has optional consecutive variables such as V ::= [ Va Vb ], where V, Va, Vb are variables
+##   - variable whose any body does not share first elements with others (then, the grammer is called LL1 grammer)
+
+
+import re
+import functools as ft
+
+
+## TODO: runtime
+TERMINALS = ['Identifier', "UnsignedInteger", "String"]
+
+
+def validate_grammer():
+    ## read the file written the grammer in
+    with open('grammer.txt') as f:
+        lines = f.readlines()
+    lines = remove_comment_lines(lines)
+
+    ## construct grammer dict
+    grammer = {}
+    for line in lines:
+        head, bodies = divide_into_head_and_bodies(line)
+        if head in grammer.keys():
+            grammer[head] += bodies
+        grammer[head] = bodies
+
+    ## validate the grammer has no body that has optional consecutive variables
+    for head, bodies in grammer.items():
+        for body in bodies:
+            body = drop_terminals(body)
+            optional_parts = re.findall(r'\[.*?\]|\{.*?\}', body)
+            for optional_part in optional_parts:
+                if has_consecutive_variables(optional_part):
+                    print( 'error: the grammer has optional consecutive non-terminals')
+                    print(f'  {optional_part} in {head} ::= {body}')
+                    return
+                
+    ## validate the grammer is LL1 grammer
+    for head, bodies in grammer.items():
+        if len(bodies) < 2:
+            continue
+        first_set_list = []
+        for body in bodies:
+            body_head = extract_head_from_body(body)
+            first_set_list.append(first(grammer, body_head))
+        universe = ft.reduce(set.union, first_set_list, set())
+        inter_set = ft.reduce(set.intersection, first_set_list, universe)
+        if inter_set != set():
+            print( 'error: the grammer is not LL1 grammer')
+            print(f'  {inter_set} in {head} ::= {" | ".join(bodies)}')
+
+    ## the grammer is strict-LL1 grammer
+    print('OK')
+
+
+def remove_comment_lines(lines: list[str]) -> list[str]:
+    return [line for line in lines if re.match(r'[^#]\S+', line)]
+
+
+def divide_into_head_and_bodies(line: str) -> tuple[str, list[str]]:
+    elems = line.split()
+    head = elems[0]
+    head = head.strip()
+    bodies = (' '.join(elems[2:])).split('|')
+    bodies = list(map(lambda s: s.strip(), bodies))
+    return head, bodies
+
+
+def drop_terminals(body: str) -> str:
+    return re.sub(r'".*?"', ' ', body)
+
+
+def has_consecutive_variables(optional_part: str) -> bool:
+    optional_part = optional_part.strip(' []\{\}')
+    return len(optional_part.split()) > 1
+
+
+def first(grammer: dict, head: str) -> set[str]:
+    if re.match(r'".*?"', head) or head in TERMINALS:
+        return set([head])
+    bodies = grammer[head]
+    first_set = set()
+    for body in bodies:
+        body_head = extract_head_from_body(body)
+        first_set = first_set.union(first(grammer, body_head))
+    return first_set
+    
+def extract_head_from_body(body: str) -> str:
+    body = re.sub(r'\[(.*?)\]', r'\1', body)
+    body = re.sub(r'\{(.*?)\}', r'\1', body)
+    return body.split()[0] 
+
+if __name__ == '__main__':
+    validate_grammer()
