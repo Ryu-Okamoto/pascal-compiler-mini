8865732b36df5912807ac7f4cab177fea8d13e10
Ryu-Okamoto <r-okamot@ist.osaka-u.ac.jp>
2024-10-23 09:47:15 +0900

    enhance validator with lexical checking

--- doc/grammer-validator.py
+++ doc/grammer-validator.py
@@ -1,22 +1,37 @@
 ## validate whether the grammer is strict-LL1 grammer
 ## strict-LL1 grammer is defined as the grammer without
-##   - body that has optional consecutive variables such as V ::= [ Va Vb ], where V, Va, Vb are variables
-##   - variable whose any body does not share first elements with others (then, the grammer is called LL1 grammer)
+## 1. body that has optional consecutive variables such as V ::= [ Va Vb ], where V, Va, Vb are variables
+## 2. variable whose any body does not share first elements with others (then, the grammer is called LL1 grammer)
 
 
 import re
 import functools as ft
 
 
-## TODO: runtime
-TERMINALS = ['Identifier', "UnsignedInteger", "String"]
+## patterns for EBNF elements
+TERMINAL_PATTERN = r'".*?"|@[A-Z][A-Za-z]*'
+VARIABLE_PATTERN = r'[A-Z][A-Za-z]*' 
+METACHAR_PATTERN = r'[\|\[\]\{\}]|::='
 
 
 def validate_grammer():
+
     ## read the file written the grammer in
     with open('grammer.txt') as f:
         lines = f.readlines()
     lines = remove_comment_lines(lines)
+
+    ## validate lexical and syntax features
+    for idx, line in enumerate(lines):
+        line_number = idx + 1
+        if not validate_lexically(line):
+            print( 'error: the grammer has lexical error')
+            print(f'  at line {line_number}: {line}') 
+            return 
+        if not validate_syntactically(line):
+            print( 'error: the grammer has syntax error')
+            print(f'  at line {line_number}: {line}')
+            return 
 
     ## construct grammer dict
     grammer = {}
@@ -29,8 +44,8 @@ def validate_grammer():
     ## validate the grammer has no body that has optional consecutive variables
     for head, bodies in grammer.items():
         for body in bodies:
-            body = drop_terminals(body)
-            optional_parts = re.findall(r'\[.*?\]|\{.*?\}', body)
+            sanitised_body = drop_terminals(body)
+            optional_parts = re.findall(r'\[.*?\]|\{.*?\}', sanitised_body)
             for optional_part in optional_parts:
                 if has_consecutive_variables(optional_part):
                     print( 'error: the grammer has optional consecutive non-terminals')
@@ -41,15 +56,17 @@ def validate_grammer():
     for head, bodies in grammer.items():
         if len(bodies) < 2:
             continue
-        first_set_list = []
+        first_sets = []
         for body in bodies:
-            body_head = extract_head_from_body(body)
-            first_set_list.append(first(grammer, body_head))
-        universe = ft.reduce(set.union, first_set_list, set())
-        inter_set = ft.reduce(set.intersection, first_set_list, universe)
+            first_elem = extract_first_elem_from_body(body)
+            first_set = get_first_set(grammer, first_elem)
+            first_sets.append(first_set)
+        universe = ft.reduce(set.union, first_sets, set())
+        inter_set = ft.reduce(set.intersection, first_sets, universe)
         if inter_set != set():
             print( 'error: the grammer is not LL1 grammer')
             print(f'  {inter_set} in {head} ::= {" | ".join(bodies)}')
+            return
 
     ## the grammer is strict-LL1 grammer
     print('OK')
@@ -57,6 +74,21 @@ def validate_grammer():
 
 def remove_comment_lines(lines: list[str]) -> list[str]:
     return [line for line in lines if re.match(r'[^#]\S+', line)]
+
+
+def validate_lexically(line: str) -> bool:
+    rest = \
+        re.sub(METACHAR_PATTERN, '',
+            re.sub(VARIABLE_PATTERN, '',
+                re.sub(TERMINAL_PATTERN, '', line)
+            )
+        )
+    return re.match(r'\S+', rest) is None
+
+
+def validate_syntactically(line: str) -> bool:
+    ## TODO
+    return True
 
 
 def divide_into_head_and_bodies(line: str) -> tuple[str, list[str]]:
@@ -69,7 +101,7 @@ def divide_into_head_and_bodies(line: str) -> tuple[str, list[str]]:
 
 
 def drop_terminals(body: str) -> str:
-    return re.sub(r'".*?"', ' ', body)
+    return re.sub(TERMINAL_PATTERN, ' ', body)
 
 
 def has_consecutive_variables(optional_part: str) -> bool:
@@ -77,17 +109,17 @@ def has_consecutive_variables(optional_part: str) -> bool:
     return len(optional_part.split()) > 1
 
 
-def first(grammer: dict, head: str) -> set[str]:
-    if re.match(r'".*?"', head) or head in TERMINALS:
+def get_first_set(grammer: dict, head: str) -> set[str]:
+    if re.match(TERMINAL_PATTERN, head):
         return set([head])
     bodies = grammer[head]
     first_set = set()
     for body in bodies:
-        body_head = extract_head_from_body(body)
-        first_set = first_set.union(first(grammer, body_head))
+        first_elem = extract_first_elem_from_body(body)
+        first_set = first_set.union(get_first_set(grammer, first_elem))
     return first_set
     
-def extract_head_from_body(body: str) -> str:
+def extract_first_elem_from_body(body: str) -> str:
     body = re.sub(r'\[(.*?)\]', r'\1', body)
     body = re.sub(r'\{(.*?)\}', r'\1', body)
     return body.split()[0] 
